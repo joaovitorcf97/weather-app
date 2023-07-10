6ebb41230d969afd86f2c7a9a14464e0767827e6
João Vitor <joaovitorcfdev@gmail.com>
2023-07-10 17:36:36 -0300

    finalize project

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  faDroplet,
+  faTemperatureHigh,
+  faTemperatureLow,
+  faWind,
+} from '@fortawesome/free-solid-svg-icons';
+import { WeatherData } from 'src/app/models/interfaces/weatherdata.interface';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: [],
+})
+export class WeatherCardComponent {
+  @Input() weatherDataInput!: WeatherData;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTecperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+}
